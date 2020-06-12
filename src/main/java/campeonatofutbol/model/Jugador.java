537fd2a429afb4ed64aac1e8827fc5cfc1fb1fac
Pedro Antonio <shadycreekrd@gmail.com>
2020-06-12 11:11:06 +0200

    Version 2.0

--- src/main/java/campeonatofutbol/model/Jugador.java
+++ src/main/java/campeonatofutbol/model/Jugador.java
@@ -2,4 +2,26 @@ package campeonatofutbol.model;
 
 public class Jugador {
 
+	private String nombre;
+	private int numDorsal;
+	public Jugador(String nombre, int numDorsal) {
+		super();
+		this.nombre = nombre;
+		this.numDorsal = numDorsal;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getNumDorsal() {
+		return numDorsal;
+	}
+	public void setNumDorsal(int numDorsal) {
+		this.numDorsal = numDorsal;
+	}
+	
+	
+		
 }
